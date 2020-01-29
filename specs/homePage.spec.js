33899ca5603748da14054095bd15f037d62d21b8
Matias - MacBook Pro <matiasbrunofornero@gmail.com>
2020-01-29 12:02:31 -0300

    added homePagaData.json

--- specs/homePage.spec.js
+++ specs/homePage.spec.js
@@ -4,7 +4,22 @@ var homePageData = require('../../data/homePageData.json');
 var homePage = require('../../pages/homePage');
 
 describe("Homepage Test Suite", function () {
-    it("Turismo City Logo Visibility", function () {
+
+    it("Page Logo Visibility", function () {
         expect(homePage.turismoCityDesktopLogo.isDisplayed()).toBe(true, 'Page logo is not displayed')
+    })
+
+    using(objectDataProvider, function (data, description) {
+        it(description + "icon verification", function () {
+            var currentElement = data.order - 1
+            expect(homePage.getIconLink(currentElement)).toContain(data.url);
+            expect(homePage.headerElements.get(currentElement).getText()).toContain(data.text)
+            expect(homePage.getIconClassName(currentElement)).toContain(data.iconClassName)
+        })
+    })
+
+    it("Default Search Box Verification", function () {
+        expect(homePage.defaultSearchBox.getText()).toContain(homePageData.defaultSearchBoxTitle)
+        expect(homePage.defaultSearchBox.isDisplayed()).toBe(true, 'Default Search box: ' + homePageData.defaultSearchBoxTitle + ' not displayed')
     })
 })

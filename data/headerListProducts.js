c33741553f662909767742850b812ea7a72faf7b
Matias - MacBook Pro <matiasbrunofornero@gmail.com>
2020-01-29 11:47:21 -0300

    added headerListProducts.js

---
+++ data/headerListProducts.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+	'Vuelos': { order: 1, text: 'Vuelos', url: 'https://www.turismocity.com.ar/vuelos', iconClassName: 'icon-vuelos' },
+	'Hoteles': { order: 2, text: 'Hoteles', url: 'https://www.turismocity.com.ar/hoteles', iconClassName: 'icon-hoteles' },
+	'Paquetes': { order: 3, text: 'Paquetes', url: 'https://www.turismocity.com.ar/paquetes', iconClassName: 'icon-paquetes' },
+	'Micros': { order: 4, text: 'Micros', url: 'https://www.turismocity.com.ar/micros', iconClassName: 'icon-micros' },
+	'Autos': { order: 5, text: 'Autos', url: 'https://www.turismocity.com.ar/autos', iconClassName: 'icon-autos' },
+    'Promociones': { order: 6, text: 'Promociones', url: 'https://www.turismocity.com.ar/promociones_aereas', iconClassName: 'icon-promociones' },
+	'Seguros de Viajes': { order: 7, text: 'Seguros de Viajes', url: 'https://www.universal-assistance.com/ar-es/turismocity', iconClassName: 'icon-assist' },
+}

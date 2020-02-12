cf05098a8a0a7c559f73d5d47b609991e621cb49
Matias - MacBook Pro <matiasbrunofornero@gmail.com>
2020-02-12 16:03:05 -0300

    Added cityList to 'Search from Inmuebles' TC

---
+++ data/cityList.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+	'Mar del Plata': { order: 1, text: 'Mar del Plata' },
+	'Buenos Aires': { order: 2, text: 'Buenos Aires' },
+	'Rosario': { order: 3, text: 'Rosario' },
+    'Cordoba': { order: 4, text: 'Cordoba' },
+}
--- specs/homePage.spec.js
+++ specs/homePage.spec.js
@@ -1,5 +1,6 @@
 var using = require('jasmine-data-provider')
-var objectDataProvider = require('../data/productsList')
+var productsData = require('../data/productsList')
+var citiesData = require('../data/cityList')
 var mlData = require('../data/homePageData.json')
 
 var homePage = require('../pages/homePage')
@@ -9,8 +10,8 @@ var inmueblesPage = require('../pages/inmueblesPage')
 
 describe("Homepage Test Suite", function () {
 
-    using(objectDataProvider, function (data, description) {
-        it("Search functionality: " + description, function () {
+    using(productsData, function (data, description) {
+        it("Perform '"+ description +"' as Basic Search", function () {
             const placeholder = mlData.searchboxPlaceholder
 
             expect(homePage.searchBox.isDisplayed()).toBe(true)
@@ -26,24 +27,26 @@ describe("Homepage Test Suite", function () {
         })
     }),
 
-        it("Inmuebles functionality from Categorias", function () {
+    using(citiesData, function (data, description) {
+        it("Search '"+ description +"' from Inmuebles", function () {
             homePage.clickCategoriasLink()
             expect(homePage.categoriasMenu.getAttribute('hidden')).not.toEqual('hidden');
             homePage.goToCategory('Inmuebles')
 
             inmueblesPage.selectOperation('Alquiler')
             inmueblesPage.selectCategory('PH')
-            inmueblesPage.setCity('Mar del Plata')
+            inmueblesPage.setCity(data.text)
 
             inmueblesPage.clickSearch()
 
             resultsPage.waitToPresenceOf(resultsPage.breadcrumbTitle) 
             expect(resultsPage.breadcrumbTitle.isDisplayed()).toBe(true)
-            expect(resultsPage.breadcrumbTitle.getText()).toEqual('Mar del Plata'.toLowerCase());
+            expect(resultsPage.breadcrumbTitle.getText()).toEqual(data.text.toLowerCase());
 
             homePage.clearSearch()
             expect(homePage.getSearch()).toEqual('');
         })
+    }),
 
     it("Ingresa functionality", function () {
         homePage.clickIngresaBtn()

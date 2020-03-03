9e7f1c9601795499d18e16daf25f73eff665bd84
Matias - MacBook Pro <matiasbrunofornero@gmail.com>
2020-03-03 15:41:44 -0300

    Uncommented old TCs

--- specs/homePage.spec.js
+++ specs/homePage.spec.js
@@ -18,45 +18,45 @@ describe("Homepage Test Suite", function () {
         ingresaPage.setPassword(data.password)
     })
 
-    // using(people, function (data, description) {
-    //     xit("Follow and Unfollow '" + description + "' Page", function () {
+    using(people, function (data, description) {
+        xit("Follow and Unfollow '" + description + "' Page", function () {
 
-    //         homePage.setSearch(data.text)
-    //         resultsPage.clickPeople()
+            homePage.setSearch(data.text)
+            resultsPage.clickPeople()
 
-    //         resultsPage.clickVerifiedAccount()
-    //         profilePage.followUnfollow('Follow')
+            resultsPage.clickVerifiedAccount()
+            profilePage.followUnfollow('Follow')
 
-    //         expect(profilePage.followBtn.getText()).toEqual('Following')
+            expect(profilePage.followBtn.getText()).toEqual('Following')
 
-    //         profilePage.followUnfollow('Following')
-    //         expect(profilePage.unfollowPopup.isDisplayed()).toBe(true)
-    //         profilePage.confirmUnfollow()
+            profilePage.followUnfollow('Following')
+            expect(profilePage.unfollowPopup.isDisplayed()).toBe(true)
+            profilePage.confirmUnfollow()
 
-    //         expect(profilePage.followBtn.getText()).toEqual('Follow')
+            expect(profilePage.followBtn.getText()).toEqual('Follow')
 
-    //         profilePage.clickHome()
-    //         expect(profilePage.homePageIsDisplayed()).toBe(true)
-    //     })
-    // })
+            profilePage.clickHome()
+            expect(profilePage.homePageIsDisplayed()).toBe(true)
+        })
+    })
 
-    // xit("Tweet about something funny that happened recently", function () {
-    //     var random = homePage.newTweet().setTweet()
-    //     homePage.sendTweet()
-    //     expect(homePage.confirmationAlert.isDisplayed()).toBe(true)
+    xit("Tweet about something funny that happened recently", function () {
+        var random = homePage.newTweet().setTweet()
+        homePage.sendTweet()
+        expect(homePage.confirmationAlert.isDisplayed()).toBe(true)
 
-    //     homePage.goToProfile()
-    //     var tweet = profilePage.getLastTweet()
-    //     expect(tweet.getText()).toEqual(tweets[['e' + [random]]])
+        homePage.goToProfile()
+        var tweet = profilePage.getLastTweet()
+        expect(tweet.getText()).toEqual(tweets[['e' + [random]]])
 
-    //     profilePage.openMoreMenu()
-    //     expect(profilePage.moreMenu.isDisplayed()).toBe(true)
-    //     profilePage.selectOption('Delete')
-    //     expect(profilePage.deletePopup.isDisplayed()).toBe(true)
+        profilePage.openMoreMenu()
+        expect(profilePage.moreMenu.isDisplayed()).toBe(true)
+        profilePage.selectOption('Delete')
+        expect(profilePage.deletePopup.isDisplayed()).toBe(true)
 
-    //     profilePage.deleteTweet()
-    //     expect(tweet.getText()).not.toEqual(tweets[['e' + [random]]])
-    // })
+        profilePage.deleteTweet()
+        expect(tweet.getText()).not.toEqual(tweets[['e' + [random]]])
+    })
 
     it("Send a message by day is important!", function () {
         var user = 'MatBF2'

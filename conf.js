5b25b5157eac2e4da24bd19134ec75f0f0efdba8
Matias - MacBook Pro <matiasbrunofornero@gmail.com>
2020-04-09 20:39:20 -0300

    Appium compatibility!

--- conf.js
+++ conf.js
@@ -1,13 +1,16 @@
 exports.config = {
-    directConnect: true,
-    specs: ['./specs/homePage.spec.js'],
+
     capabilities: {
         'browserName': 'chrome'
     },
+
+    directConnect: true,
     framework: 'jasmine',
+    specs: ['./specs/homePage.spec.js'],
     jasmineNodeOpts: {
         defaultTimeoutInterval: 50000
     },
+
     onPrepare: () => {
         browser.ignoreSynchronization = true
         browser.driver.manage().window().maximize()
--- specs/homePage.spec.js
+++ specs/homePage.spec.js
@@ -9,7 +9,6 @@ var profilePage = require('../pages/profilePage')
 var resultsPage = require('../pages/resultsPage')
 var messagesPage = require('../pages/messagesPage')
 var followingPage = require('../pages/followingPage')
-
 var sidebar = require('../pages/common/sidebar')
 var msgPopup = require('../pages/components/msgPopup')
 
